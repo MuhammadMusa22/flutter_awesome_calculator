8f0f5c8a34dcede92e97291c014d08292de093a1
Muhammad Musa <42924055+MuhammadMusaaa@users.noreply.github.com>
2022-08-23 14:35:24 +0500

    README.md updated

--- test/calculator_test.dart
+++ test/calculator_test.dart
@@ -4,7 +4,7 @@ import 'package:calculator/flutter_awesome_calculator.dart';
 
 void main() {
   test('adds one to input values', () {
-    final calculator = Calculator();
+    // final calculator = Calculator();
     // expect(calculator.addOne(2), 3);
     // expect(calculator.addOne(-7), -6);
     // expect(calculator.addOne(0), 1);
